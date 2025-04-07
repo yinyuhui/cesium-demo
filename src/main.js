fb4b952cc81ce26b160f5aa3248be668c93f5933
yinyuhui <veinyin@gmail.com>
2025-04-07 10:23:09 +0800

    feat: add cesium

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
-import 'cesium/Build/Cesium/Widgets/widgets.css'
 import App from './App.vue'
-window.CESIUM_BASE_URL = './cesium/'
+import * as Cesium from "cesium";
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4YmM3MmUyNS03ZTMxLTRiZmItOGQ2My03ZjkwZGQzNzFlYmUiLCJpZCI6MjkwMjY4LCJpYXQiOjE3NDM1ODY2OTd9.Bm9wezicSFRbYRHbu7Jfnk3ZX4thpiIt-6ki7hdOsLo'
+// window.CESIUM_BASE_URL = './CesiumAssets/'
+// window.CESIUM_BASE_URL = './Build/Cesium/'
 createApp(App).mount('#app')
